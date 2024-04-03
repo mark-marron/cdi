de2130a5bc5814078318abc1664243a61052f281
mark-marron <markmarron16@gmail.com>
2024-04-03 18:23:27 +0100

    fixed file reading - removed json

--- rpmCounter1/rpmCounter1.ino
+++ rpmCounter1/rpmCounter1.ino
@@ -19,18 +19,19 @@ int max_rpm = 6000;
 
 // safety properties
 int level = 2;
-int hour_levels[2] = {5, 10};
-int hours = 4;
+int level_requirements[2] = {5, 10};
+int seconds = 3600;
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("=========================================================");
   pinMode(sparkPin, OUTPUT);
 
   if (!SD.begin(chipSelect)) {
     Serial.println("SD Card initialization failed");
     return;
   }
-  File engine_profile = SD.open("ENGINE~1.JSO");
+  File engine_profile = SD.open("ENGINE.PRO");
   File safety_profile = SD.open("SAFETY.PRO");
   if (!engine_profile || !safety_profile) {
     Serial.println("Failed to read profiles");
@@ -48,40 +49,60 @@ void setup() {
   encoded_safety_profile[i] = "\0";
   safety_profile.close();
 
-  Serial.println(encoded_safety_profile);
+  // decode base64 safety profile
   char encodedData = encoded_safety_profile;
   int encodedDataLength = strlen(encoded_safety_profile);
   int decodedDataLength = Base64.decodedLength(encodedData, encodedDataLength);
   char decodedData[decodedDataLength + 1];
   Base64.decode(decodedData, encoded_safety_profile, encodedDataLength);
-  Serial.println(decodedData);
-  String decodedSafety
+  String decoded_safety_profile = decodedData;
 
-  const size_t bufferSize = JSON_OBJECT_SIZE(3) + 30;
-  DynamicJsonDocument jsonBuffer(bufferSize);
-  DeserializationError error = deserializeJson(jsonBuffer, engine_profile);
-  if (error) {
-    Serial.println("Failed to parse engine profile JSON");
-    return;
+  // assign engine data to variables
+  if (engine_profile.available()) {
+    voltage_threshold = engine_profile.parseInt();
+    engine_profile.read();
+    number_of_cylinders =  engine_profile.parseInt();
+    engine_profile.read();
+    max_rpm =  engine_profile.parseInt();
   }
-  JsonObject engine = jsonBuffer.as<JsonObject>();
+  engine_profile.close();
 
-  DynamicJsonDocument decodedJsonBuffer(bufferSize);
-  DeserializationError decodedError = deserializeJson(decodedJsonBuffer, decodedData);
-  if (decodedError) {
-    Serial.println("Failed to parse safety profile JSON");
-    return;
+  // assign safety data to variables
+  int i1 = decoded_safety_profile.indexOf("\n");
+  int i2 = decoded_safety_profile.indexOf("\n", i1 + 1);
+  int i3 = decoded_safety_profile.indexOf("\n", i2 + 1);
+  int l1 = decoded_safety_profile.substring(0, i1).toInt();
+  int l2 = decoded_safety_profile.substring(i1, i2).toInt();
+  seconds = decoded_safety_profile.substring(i2, i3).toInt();
+  level_requirements[0] = l1;
+  level_requirements[1] = l2;
+
+  if (true) {
+    Serial.println("Engine Info:");
+    Serial.println(voltage_threshold);
+    Serial.println(number_of_cylinders);
+    Serial.println(max_rpm);
+    Serial.println("Safety Info:");
+    Serial.println(seconds);
+    Serial.print("{");
+    for (int i=0; i<=sizeof(level_requirements) / sizeof(level_requirements[0]); i++) {
+      Serial.print(level_requirements[i]);
+      Serial.print(" ");
+    }
+    Serial.println("}");
   }
-  level = decodedJsonBuffer["level"];
 
-  voltage_threshold = engine["voltage_threshold"];
-  number_of_cylinders = engine["number_of_cylinders"];
-  max_rpm = engine["max_rpm"];
+  // calculate level
+  int size = sizeof(level_requirements) / sizeof(level_requirements[0]);
+  level = 1;
+  for (int i=0; i<=size; i++) {
+    if (seconds > level_requirements[i]) {
+      level ++;
+    }
+  }
 
-  Serial.println(voltage_threshold);
-  Serial.println(number_of_cylinders);
-  Serial.println(max_rpm);
-  Serial.println(level);
+  // calculate maxRPM
+  max_rpm = 3000 + (level/3) * (max_rpm - 3000);
 }
 
 void loop() {
